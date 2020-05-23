246880e350325a606a13492e3a936a853a26c5af
Ricardo Venegas <44240126+ricardov-venegas-intl@users.noreply.github.com>
2020-05-23 16:59:03 -0500

    Issue 2 - Create simple markdown parser (#8)
    * Add Color Parser for Markdown.
    * Add unit tests for markdown
    * Fix Github workflow

---
+++ VenegasIntl.SecretMarkdown.Backend/ColorParser/ColorStyle.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ColorStyle.cs" company="Ricardo Venegas / Venegas International, LLC">
+// Copyright (c) Ricardo Venegas / Venegas International, LLC. All rights reserved.
+// </copyright>
+
+namespace VenegasIntl.SecretMarkdown.Backend.ColorParser
+{
+    /// <summary>
+    /// Color Styles for the Markdown.
+    /// </summary>
+    public enum ColorStyle
+    {
+        /// <summary>
+        /// Header 1 color style.
+        /// </summary>
+        Header1,
+
+        /// <summary>
+        /// Header 2 color style.
+        /// </summary>
+        Header2,
+
+        /// <summary>
+        /// Header 3 color style.
+        /// </summary>
+        Header3,
+
+        /// <summary>
+        /// LIst or enumeration prefix color style.
+        /// </summary>
+        ListPrefix,
+
+        /// <summary>
+        /// Normal text color style.
+        /// </summary>
+        Normal,
+    }
+}
