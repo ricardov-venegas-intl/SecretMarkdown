003d264e69f81191b104086e0c4e719731c1541c
Ricardo Venegas <ricardov@venegas-intl.com>
2020-05-23 17:56:39 -0500

    Creates AES Encryptor Changes: * Add AES Text Encryptor. * Add Unit Tests.
    Resolves 3

---
+++ VenegasIntl.SecretMarkdown.Backend/Encryptor/AesTextEncryptor.cs
@@ -0,0 +1,138 @@
+﻿// <copyright file="AesTextEncryptor.cs" company="Ricardo Venegas / Venegas International, LLC">
+// Copyright (c) Ricardo Venegas / Venegas International, LLC. All rights reserved.
+// </copyright>
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VenegasIntl.SecretMarkdown.Backend.Encryptor
+{
+    /// <summary>
+    /// Encrypts a text using AES in CBC mode.
+    /// </summary>
+    public class AesTextEncryptor : ITextEncryptor
+    {
+        /// <summary>
+        /// Descrypts a text with a password.
+        /// </summary>
+        /// <param name="encryptedData">A byte array with the encrypted text.</param>
+        /// <param name="password">Password to decrypt.</param>
+        /// <returns>The decrypted text.</returns>
+        public string DecryptText(byte[] encryptedData, string password)
+        {
+            if (encryptedData == null)
+            {
+                throw new ArgumentNullException(nameof(encryptedData));
+            }
+
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            if (password.Length == 0)
+            {
+                throw new ArgumentException("Invalid password");
+            }
+
+            if (encryptedData.Length < 512)
+            {
+                throw new ArgumentException("Encrypted data is too small");
+            }
+
+            // Separate salt from the encrypted content
+            var salt = encryptedData.Take(256).ToArray();
+            var encryptedContent = encryptedData.Skip(256).ToArray();
+
+            // Generate the aes keys from the password, iteration count is larger for smaller passwords
+            int iterationCount = 1023 + (Math.Max(63, 127 - password.Length) * 31);
+            using (var pdb = new Rfc2898DeriveBytes(password, salt, iterationCount, HashAlgorithmName.SHA512))
+            {
+                using (var ms = new MemoryStream(encryptedContent))
+                {
+                    using (Aes aes = new AesManaged())
+                    {
+                        aes.Mode = CipherMode.CBC;
+                        var key = pdb.GetBytes(aes.KeySize / 8);
+                        var iv = pdb.GetBytes(aes.BlockSize / 8);
+                        aes.Key = key;
+                        aes.IV = iv;
+                        CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+                        var decryptedData = new byte[encryptedContent.Length];
+                        var bytesDecrypted = cs.Read(decryptedData, 0, decryptedData.Length);
+                        cs.Close();
+                        var contentWithoutFiller = decryptedData.Skip(256).Take(bytesDecrypted).ToArray();
+                        return Encoding.UTF8.GetString(contentWithoutFiller).TrimEnd('\0');
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Encrypts a text with a password.
+        /// </summary>
+        /// <param name="content">Content to encrypt.</param>
+        /// <param name="password">Password to encrypt.</param>
+        /// <returns>A byte array with the encrypted text.</returns>
+        public byte[] EncryptText(string content, string password)
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            if (password.Length == 0)
+            {
+                throw new ArgumentException("Invalid password");
+            }
+
+            using (RNGCryptoServiceProvider randomGenerator = new RNGCryptoServiceProvider())
+            {
+                // Generate ramdom salt
+                var salt = new byte[256];
+                randomGenerator.GetBytes(salt);
+
+                // Create a random prefix for the content
+                var filler = new byte[256];
+                randomGenerator.GetBytes(filler);
+
+                // Convert the text to bytes
+                var clearBytes = Encoding.UTF8.GetBytes(content);
+
+                // Generate the aes keys from the password, iteration count is larger for smaller passwords
+                int iterationCount = 1023 + (Math.Max(63, 127 - password.Length) * 31);
+                using (var pdb = new Rfc2898DeriveBytes(password, salt, iterationCount, HashAlgorithmName.SHA512))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        ms.Write(salt, 0, salt.Length);
+                        using (Aes aes = new AesManaged())
+                        {
+                            aes.Mode = CipherMode.CBC;
+                            var key = pdb.GetBytes(aes.KeySize / 8);
+                            var iv = pdb.GetBytes(aes.BlockSize / 8);
+                            aes.Key = key;
+                            aes.IV = iv;
+
+                            // Encrypt  random filler + content
+                            CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                            cs.Write(filler, 0, filler.Length);
+                            cs.Write(clearBytes, 0, clearBytes.Length);
+                            cs.Close();
+                            ms.Flush();
+                            return ms.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ VenegasIntl.SecretMarkdown.Tests/AesTextEncryptorTests.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="AesTextEncryptorTests.cs" company="Ricardo Venegas / Venegas International, LLC">
+// Copyright (c) Ricardo Venegas / Venegas International, LLC. All rights reserved.
+// </copyright>
+
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VenegasIntl.SecretMarkdown.Backend.Encryptor;
+
+namespace VenegasIntl.SecretMarkdown.Tests
+{
+    /// <summary>
+    /// Aes Text Encryptor Tests.
+    /// </summary>
+    [TestClass]
+    public class AesTextEncryptorTests
+    {
+        /// <summary>
+        /// Simple Round Trip test.
+        /// </summary>
+        [TestMethod]
+        public void SimpleRoundTrip()
+        {
+            var secretText = "Hello World";
+            var password = "MySecretPassword";
+            ITextEncryptor textEncryptor = new AesTextEncryptor();
+            var encryptedText = textEncryptor.EncryptText(secretText, password);
+            Assert.IsTrue(encryptedText.Length > 512);
+            var decriptedText = textEncryptor.DecryptText(encryptedText, password);
+            Assert.AreEqual(secretText, decriptedText);
+        }
+
+        /// <summary>
+        /// Simple Round Trip test.
+        /// </summary>
+        [TestMethod]
+        public void EmptyContent()
+        {
+            var secretText = string.Empty;
+            var password = "MySecretPassword";
+            ITextEncryptor textEncryptor = new AesTextEncryptor();
+            var encryptedText = textEncryptor.EncryptText(secretText, password);
+            Assert.IsTrue(encryptedText.Length > 512);
+            var decriptedText = textEncryptor.DecryptText(encryptedText, password);
+            Assert.AreEqual(secretText, decriptedText);
+        }
+
+        /// <summary>
+        /// Very long password test.
+        /// </summary>
+        [TestMethod]
+        public void VeryLongPassword()
+        {
+            var secretText = @"Contrary to popular belief, Lorem Ipsum is not simply random text. 
+It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+and going through the cites of the word in classical literature, 
+discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of ""de Finibus Bonorum et Malorum""
+(The Extremes of Good and Evil) by Cicero, written in 45 BC. 
+This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 
+""Lorem ipsum dolor sit amet.."", comes from a line in section 1.10.32.";
+            var password = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non faucibus tortor, vel placerat risus. Etiam a feugiat elit. Donec convallis dui ut dignissim dignissim. Nam quis efficitur sem. Morbi ipsum ex, ultrices eu turpis sed, elementum semper nisi.";
+            ITextEncryptor textEncryptor = new AesTextEncryptor();
+            var encryptedText = textEncryptor.EncryptText(secretText, password);
+            Assert.IsTrue(encryptedText.Length > 512);
+            var decriptedText = textEncryptor.DecryptText(encryptedText, password);
+            Assert.AreEqual(secretText, decriptedText);
+        }
+
+        /// <summary>
+        /// Very long password test.
+        /// </summary>
+        [TestMethod]
+        public void TestValidartions()
+        {
+            var secretText = "Hello World";
+            var password = "MySecretPassword";
+            ITextEncryptor textEncryptor = new AesTextEncryptor();
+            var encryptedText = textEncryptor.EncryptText(secretText, password);
+
+            // Test EncryptText validations
+            Assert.ThrowsException<ArgumentNullException>(() => { _ = textEncryptor.EncryptText(null, password); });
+            Assert.ThrowsException<ArgumentNullException>(() => { _ = textEncryptor.EncryptText(secretText, null); });
+            Assert.ThrowsException<ArgumentNullException>(() => { _ = textEncryptor.EncryptText(null, null); });
+            Assert.ThrowsException<ArgumentException>(() => { _ = textEncryptor.EncryptText(secretText, string.Empty); });
+
+            // Test DecryptText validarions
+            Assert.ThrowsException<ArgumentNullException>(() => { _ = textEncryptor.DecryptText(null, password); });
+            Assert.ThrowsException<ArgumentNullException>(() => { _ = textEncryptor.DecryptText(encryptedText, null); });
+            Assert.ThrowsException<ArgumentNullException>(() => { _ = textEncryptor.DecryptText(null, null); });
+            Assert.ThrowsException<ArgumentException>(() => { _ = textEncryptor.DecryptText(encryptedText, string.Empty); });
+            Assert.ThrowsException<ArgumentException>(() => { _ = textEncryptor.DecryptText(encryptedText.Take(511).ToArray(), password); });
+        }
+    }
+}
