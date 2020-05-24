9f4214654d382c9699ac204f9ce51269e93c9954
Ricardo Venegas <ricardov@venegas-intl.com>
2020-05-23 19:19:23 -0500

    Issue 4 Create Notes respository. Changes: * Add notes repository. * Add unit tests
    Resolves #4

---
+++ VenegasIntl.SecretMarkdown.Backend/Repositories/INotesRepository.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="INotesRepository.cs" company="Ricardo Venegas / Venegas International, LLC">
+// Copyright (c) Ricardo Venegas / Venegas International, LLC. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VenegasIntl.SecretMarkdown.Backend.Repositories
+{
+    /// <summary>
+    /// Notes repository interface.
+    /// </summary>
+    public interface INotesRepository
+    {
+        /// <summary>
+        /// Indicates if there is aleady notes available.
+        /// </summary>
+        /// <returns>True if notes are available.</returns>
+        bool AreNotesPresent();
+
+        /// <summary>
+        /// Saves the notes.
+        /// </summary>
+        /// <param name="encryptedNotes">Notes already encrypted.</param>
+        void SaveNotes(byte[] encryptedNotes);
+
+        /// <summary>
+        /// Loads the notes.
+        /// </summary>
+        /// <returns>Encryped notes.</returns>
+        byte[] LoadNotes();
+    }
+}
---
+++ VenegasIntl.SecretMarkdown.Backend/Repositories/NotesRepository.cs
@@ -0,0 +1,94 @@
+﻿// <copyright file="NotesRepository.cs" company="Ricardo Venegas / Venegas International, LLC">
+// Copyright (c) Ricardo Venegas / Venegas International, LLC. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VenegasIntl.SecretMarkdown.Backend.Repositories
+{
+    /// <summary>
+    /// Notes repository.
+    /// Stores notes in OneDrive if present otherwise in AppData.
+    /// </summary>
+    public class NotesRepository : INotesRepository
+    {
+        private string repositoryFolder;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotesRepository"/> class.
+        /// </summary>
+        public NotesRepository()
+            : this("notest.secret")
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotesRepository"/> class.
+        /// </summary>
+        /// <param name="fileName">File name of the notes file.</param>
+        public NotesRepository(string fileName)
+        {
+            if (Environment.GetEnvironmentVariable("OneDrive") != null)
+            {
+                this.repositoryFolder = Path.Combine(Environment.GetEnvironmentVariable("OneDrive"), "SecretMarkdown");
+            }
+            else
+            {
+                this.repositoryFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SecretMarkdown");
+            }
+
+            this.RepositoryFilePath = Path.Combine(this.repositoryFolder, fileName);
+        }
+
+        /// <summary>
+        /// Gets or sets the RepositoryFilePath (used to test implementation).
+        /// </summary>
+        public string RepositoryFilePath { get; set; }
+
+        /// <summary>
+        /// Indicates if there is aleady notes available.
+        /// </summary>
+        /// <returns>True if notes are available.</returns>
+        public bool AreNotesPresent()
+        {
+            return File.Exists(this.RepositoryFilePath);
+        }
+
+        /// <summary>
+        /// Saves the notes.
+        /// </summary>
+        /// <param name="encryptedNotes">Notes already encrypted.</param>
+        public void SaveNotes(byte[] encryptedNotes)
+        {
+            if (encryptedNotes == null)
+            {
+                throw new ArgumentNullException(nameof(encryptedNotes));
+            }
+
+            if (Directory.Exists(this.repositoryFolder) == false)
+            {
+                Directory.CreateDirectory(this.repositoryFolder);
+            }
+
+            File.WriteAllBytes(this.RepositoryFilePath, encryptedNotes);
+        }
+
+        /// <summary>
+        /// Loads the notes.
+        /// </summary>
+        /// <returns>Encryped notes.</returns>
+        public byte[] LoadNotes()
+        {
+            if (File.Exists(this.RepositoryFilePath) == false)
+            {
+                throw new InvalidOperationException("Notes doesn't exist");
+            }
+
+            return File.ReadAllBytes(this.RepositoryFilePath);
+        }
+    }
+}
